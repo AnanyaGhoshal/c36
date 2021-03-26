9db751790833fe77c1333a8d33884259ba50951b
AnanyaGhoshal <76252188+AnanyaGhoshal@users.noreply.github.com>
2021-03-26 19:12:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var form, game, player, playerCount;
+var database;
+var gameState = 0;
+
+
+function setup(){
+
+    database = firebase.database();
+
+    createCanvas(400,400);
+    
+
+
+}
+
+function draw(){
+    background("white");
+}
